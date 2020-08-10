a932a9abc5ce7d7394e7f2542433e5cbb488a572
Halit <jhalitaksoy@gmail.com>
2020-08-10 16:13:29 +0300

    Initial commit

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({Key key}) : super(key: key);
+
+  @override
+  HomePageState createState() => HomePageState();
+}
+
+class HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Center(child: Text("Home Page")),
+      ),
+    );
+  }
+}
